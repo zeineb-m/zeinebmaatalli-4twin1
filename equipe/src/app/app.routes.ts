8a9e7cfa8c10bd0bb6d6a61bba6465990ff682eb
zeineb maatalli <157981311+zeineb-m@users.noreply.github.com>
2025-04-13 18:53:03 +0100

    Update app.routes.ts

--- equipe/src/app/app.routes.ts
+++ equipe/src/app/app.routes.ts
@@ -2,13 +2,6 @@ import { Routes } from '@angular/router';
 import { ListEquipeComponent } from './list-equipe/list-equipe.component';
 
 export const routes: Routes = [
-  {
-    path: 'list',
-    component: ListEquipeComponent
-  },
-  {
-    path: '',
-    redirectTo: 'list',
-    pathMatch: 'full'
-  }
+  { path: 'list', component: ListEquipeComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
 ];
